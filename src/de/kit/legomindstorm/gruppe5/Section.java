8942faf4a91a9d29a52408ce363d95ea9dcce56d
Unknown <alex-w93@gmx.de>
2018-10-25 22:51:44 +0200

    Updated the Enum Section
    Added a description for each entry.

--- src/de/kit/legomindstorm/gruppe5/Section.java
+++ src/de/kit/legomindstorm/gruppe5/Section.java
@@ -4,19 +4,25 @@ package de.kit.legomindstorm.gruppe5;
  * Enumeration containing all sections of the course with their identifiers.
  */
 public enum Section {
-	LINE_FOLLOWING(1),
-	BRIDGE(2),
-	LABYRINTH(3),
-	PUSH_BOX(4);
+	LINE_FOLLOWING(1, "Follow the line"),
+	BRIDGE(2, "Bridge"),
+	LABYRINTH(3, "Labyrinth"),
+	PUSH_BOX(4, "Push the Box");
 	
 	private final int id;
+	private final String description;
 	
-	Section(int id) {
+	Section(int id, String description) {
 		this.id = id;
+		this.description = description;
 	}
 	
 	public int id() {
 		return id;
+	}
+	
+	public String description() {
+		return description;
 	}
 	
 }
